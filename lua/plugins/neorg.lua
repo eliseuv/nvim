82ede3547bac6fb49698819fb638f1172ebe522e
evf <eliseuv816@gmail.com>
2025-05-13 21:27:11 -0300

    [neorg] install neorg treesitter grammar

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,16 +1,18 @@
 return {
-    -- -- LuaRocks
-    -- {
-    --     "vhyrro/luarocks.nvim",
-    --     priority = 1000, -- We'd like this plugin to load first out of the rest
-    --     config = true, -- This automatically runs `require("luarocks-nvim").setup()`
-    -- },
 
-    -- neorg
-    {
-        "nvim-neorg/neorg",
-        lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-        version = "*", -- Pin Neorg to the latest stable release
-        config = true,
-    },
+  -- neorg
+  {
+    "nvim-neorg/neorg",
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = true,
+  },
+
+  -- Tree-sitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "norg")
+    end,
+  },
 }

bed1980562b517c0093fa7c83845b093eb87d7ef
evf <eliseuv816@gmail.com>
2025-05-17 22:08:59 -0300

    [flake] add project flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Flake for the Neovim config files";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+    }@inputs:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+
+            # Lua
+            lua51Packages.lua
+
+            # LSP
+            lua-language-server
+
+            # Formatter
+            stylua
+
+            # Lua Rocks
+            lua51Packages.luarocks
+
+          ];
+
+          shellHook = ''
+            lua -v
+          '';
+        };
+
+      }
+    );
+
+}

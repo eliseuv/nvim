66dd6c12464ea9c60601dc803d61b7316fbae45f
evf <eliseuv816@gmail.com>
2025-06-04 10:36:46 -0300

    [mason] unpin version in my config since its already pinned by LazyVim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,17 +4,9 @@ return {
     "williamboman/mason.nvim",
     -- Disable mason
     enabled = false,
-    -- Pin version
-    version = "^1.0.0",
-    -- enabled = false,
     opts = {
       -- Prefer local binaries over mason binaries
       PATH = "append",
     },
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    -- Pin version
-    version = "^1.0.0",
   },
 }

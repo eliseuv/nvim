cca7149e5c66f751c3629ddc407dafeb4eb8c18a
evf <eliseuv816@gmail.com>
2025-10-09 17:43:35 -0300

    [ai] config sidekick.nvim

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,23 @@
+return {
+
+  {
+    "folke/sidekick.nvim",
+    opts = {
+      cli = {
+        mux = {
+          backend = "tmux",
+          enabled = true,
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>ag",
+        function()
+          require("sidekick.cli").toggle({ name = "gemini", focus = true })
+        end,
+        desc = "Sidekick Toggle Gemini",
+      },
+    },
+  },
+}

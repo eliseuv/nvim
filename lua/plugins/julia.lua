95d729fee854aa7aa8e88539b353a1633221aacd
evf <eliseuv@pm.me>
2025-12-09 11:16:07 -0300

    [julia] remove explicit tree-sitter installation

--- lua/plugins/julia.lua
+++ lua/plugins/julia.lua
@@ -1,13 +1,5 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "julia",
-      })
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
     dependencies = { "JuliaEditorSupport/julia-vim" },
     ---@class PluginLspOpts

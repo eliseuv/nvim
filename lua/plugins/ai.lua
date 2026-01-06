d7920cd0b643b9392d139d1cc5d6d65bbfa93e92
evf <eliseuv@pm.me>
2026-01-06 16:42:48 -0300

    [sidekick] add prompt: scientific

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -7,6 +7,8 @@ return {
         prompts = {
           -- Propose implementation
           implement = "Popose an implementation for {this}. Use the containing comments as instructions on the desired behavior. Also take into cosideration the overall project style and idiom.",
+          -- Scientific writing
+          scientific = "Assume this is scientific text. If needed, rewrite parts of {this} for grammar, spelling, clarity and style always mantaining the original meaning. If there is information that needs to be added let me know but do not add it yourself.",
         },
         mux = {
           backend = "tmux",

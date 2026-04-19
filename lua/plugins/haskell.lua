26a48452116035aaee7bf946683c418db7d59060
evf <eliseuv@pm.me>
2026-04-19 20:45:58 -0300

    [haskell] fix eval-all command

---
+++ lua/plugins/haskell.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "mrcjkb/haskell-tools.nvim",
+        keys = {
+            {
+                "<localleader>e",
+                "<cmd>Hls EvalAll<cr>",
+                ft = "haskell",
+                desc = "Evaluate All",
+            }
+        }
+    }
+}
